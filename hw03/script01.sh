400eaf2ff3ab54c34252fd58b53cbbe23bceedbd
kolai23 <q3181309@gmail.com>
2023-07-13 00:28:07 +0300

    update

--- hw03/script01.sh
+++ hw03/script01.sh
@@ -1,3 +1,7 @@
 #получение и форматирование IP
 ip add | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | sed 's/inet/ИП/g'
 df -h | awk '{print $1,$4}' | sed 's/Filesystem Avail/Файловая_система  Доступно/g'
+cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}')
+memUsage=$(free -m | awk '/Mem/{print $3}')
+echo "Процесор: $cpuUsage%"
+echo "Оперативная память: $memUsage MB"
