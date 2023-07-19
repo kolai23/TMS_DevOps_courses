ccf09e4dd051478804822ef72015b66e6b9dff24
kolai23 <q3181309@gmail.com>
2023-07-20 01:34:50 +0300

    add

--- hw03/script01.sh
+++ hw03/script01.sh
@@ -1,7 +1,7 @@
-#получение и форматирование IP
+#!/bin/bash
 ip add | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | sed 's/inet/ИП/g'
 df -h | awk '{print $1,$4}' | sed 's/Filesystem Avail/Файловая_система  Доступно/g'
 cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}')
-memUsage=$(free -m | awk '/Mem/{print $3}')
+memUsage=$(free -m | awk '/Mem/ {print $3}')
 echo "Процесор: $cpuUsage%"
 echo "Оперативная память: $memUsage MB"
--- hw03/script02.sh
+++ hw03/script02.sh
@@ -1,7 +1,12 @@
-#получение и форматирование IP
-ip add | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | sed 's/inet/ИП/g'
-df -h | awk '{print $1,$4}' | sed 's/Filesystem Avail/Файловая_система  Доступно/g'
-cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}')
-memUsage=$(free -m | awk '/Mem/{print $3}')
-echo "Процесор: $cpuUsage%"
-echo "Оперативная память: $memUsage MB"
+#!/bin/bash
+useradd -m $1
+passwd $1
+uname -rv > /home/$1/sysinfo
+echo "================" >> /home/$1/sysinfo
+lscpu | grep -P '(^CPU\(s\):|Model name|CPU MHz)' >> /home/$1/sysinfo
+echo "================" >> /home/$1/sysinfo
+free -h >> /home/$1/sysinfo
+echo "================" >> /home/$1/sysinfo
+lsblk >> /home/$1/sysinfo
+~
+
